a104a409a0b8d206fb897119e503bdaac97c108c
manormasharma05 <manorma@hackerearth.com>
2024-06-12 11:49:13 +0530

    made changes in login file

--- src/components/Login.js
+++ src/components/Login.js
@@ -52,14 +52,13 @@ const Login = () => {
                 setErrorMessage(error.message)
             });
         }
-
     }
     
   return (
     <>  
         <Header/>
         <div className='flex h-screen items-center justify-center bg-login-bg-banner'>
-            <form onSubmit={(e)=> e.preventDefault()} className='p-12 bg-black h-[500px] w-[450px] flex flex-col text-white bg-opacity-90'>
+            <form onSubmit={(e)=> e.preventDefault()} className='p-12 bg-black w-[450px] flex flex-col text-white bg-opacity-90'>
                 <h3 className='text-slate-300 mb-5 text-2xl'>{isSigninForm ? "Sign In" : "Sign Up"}</h3>
                 {!isSigninForm  && 
                     <input ref={name} type='text' placeholder='Name' className='p-3 mb-3 bg-gray-700' />
@@ -69,17 +68,6 @@ const Login = () => {
                 <input ref={password} type='password' placeholder='Password' className='p-3 mb-5 bg-gray-700' />
                 <p className='text-slate-300 mb-3'>{errorMessage}</p>
                 <button className='p-4 bg-brand-red mb-5' onClick={handleSubmitButton}>{isSigninForm ? "Sign In" : "Sign Up"}</button>
-                <div className='flex justify-between mb-10'>
-                {isSigninForm  &&
-                    <div>
-                        <input type='checkbox' className='mr-2'/>
-                        <label className='text-slate-300'>Remember me?</label>
-                    </div>
-                }
-                    <div>
-                        <p className='text-slate-300'>Need Help?</p>
-                    </div>
-                </div>
                 <div>
                     <p className='text-slate-300' onClick={toggleSignIn} >{isSigninForm ? "New to Netflix? Signup Now" : "Already Registered? Sign in Now"}</p>
                 </div>
